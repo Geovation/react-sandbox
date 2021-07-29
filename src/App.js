95876e632e36c010fb316051b07148ac6f6ed391
Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>
2021-07-29 22:14:37 +0100

    Update app and homepage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import { Container, Grid } from '@material-ui/core';
-import HomePage from './components/home-page';
-import { useTheme } from '@material-ui/core/styles';
-
+import { Container, Grid } from "@material-ui/core";
+import HomePage from "./components/home-page";
+import { useTheme } from "@material-ui/core/styles";
 
 function App() {
-
   const theme = useTheme();
 
   return (
     <Container maxWidth="lg">
-
-      <Grid container
-        align="center"
-        justifyContent="center"
+      <Grid
+        container
         direction="column"
-        style={{ backgroundColor: '#ffa726' }}>
+        justifyContent="center"
+        alignItems="center"
+        spacing={3}
+        style={{ backgroundColor: "#ffa726" }}
+      >
         <HomePage />
       </Grid>
-      
     </Container>
-
-
-
-
   );
 }
 
--- src/components/home-page.js
+++ src/components/home-page.js
@@ -1,35 +1,26 @@
-import { Grid, Typography } from '@material-ui/core';
+import { Fragment } from "react";
+import { Grid, Typography } from "@material-ui/core";
 
 function HomePage() {
+  return (
+    <Fragment>
+      <Grid item>
+        <Typography variant="h1">React Sandbox</Typography>
+      </Grid>
 
-    return (
-        <Grid>
-
-            <Grid item >
-                <Typography align="center" variant="h1">
-                    React Sandbox
-                </Typography>
-            </Grid>
-
-            <Grid item>
-                <Typography align="right" variant="h6">
-                    from Geovation
-                </Typography>
-
-            </Grid>
-
-            <Grid item>
-                <Typography align="center" variant="h5">
-                    A project to showcase what's possible in React.
-                </Typography>
-
-            </Grid>
+      <Grid item container lg={7} justifyContent="flex-end">
+        <Grid item>
+          <Typography variant="h6">from Geovation</Typography>
         </Grid>
+      </Grid>
 
-
-
-    );
-
+      <Grid item>
+        <Typography variant="h5">
+          A project to showcase what's possible in React.
+        </Typography>
+      </Grid>
+    </Fragment>
+  );
 }
 
 export default HomePage;
