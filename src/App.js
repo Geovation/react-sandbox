47ef03cdbfc918f5db3cc74cbf20e27b32a553e5
Abdul Omar Faruq <18485958+mysticeti@users.noreply.github.com>
2021-07-29 23:27:20 +0100

    Add material ui + basic home page (#36)
    Co-authored-by: Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import "./App.css";
+import { Container, Grid } from "@material-ui/core";
+import HomePage from "./components/home-page";
+import { useTheme } from "@material-ui/core/styles";
 
 function App() {
+  const theme = useTheme();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World from Geovation Engineering team.
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={3}
+        style={{ backgroundColor: "#ffa726" }}
+      >
+        <HomePage />
+      </Grid>
+    </Container>
   );
 }
 
