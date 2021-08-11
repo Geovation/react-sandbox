907acbc79153ad3304715a51209cd815d7ad5f8d
Abdul Omar Faruq <abdulomarfaruq@gmail.com>
2021-08-11 16:56:04 +0100

    stretch google maps window

--- src/components/google-map/google-map-page.jsx
+++ src/components/google-map/google-map-page.jsx
@@ -1,4 +1,6 @@
 import GoogleMapReact from 'google-map-react';
+import { Grid, } from "@material-ui/core";
+import { Fragment } from 'react';
 
 function GoogleMapsPage() {
   const defaultProps = {
@@ -10,14 +12,26 @@ function GoogleMapsPage() {
   };
 
   return (
-    <div style={{ height: '80vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyDf6rgN-gGoCPLP6G_NHqVDAoLZ7UN2Yec" }}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-      </GoogleMapReact>
-    </div>
+    <Fragment>
+      <Grid item container>
+        <div style={{ height: '4vh', width: '100%' }}></div>
+      </Grid>
+      <Grid item container justifyContent="center" alignItems="stretch" alignContent="stretch">
+        <div style={{ height: '90vh', width: '100vw' }}>
+          <div style={{ height: '100%', width: '100vw' }}>
+            <GoogleMapReact
+              bootstrapURLKeys={{ key: "AIzaSyDf6rgN-gGoCPLP6G_NHqVDAoLZ7UN2Yec" }}
+              defaultCenter={defaultProps.center}
+              defaultZoom={defaultProps.zoom}
+            >
+            </GoogleMapReact>
+          </div>
+        </div>
+      </Grid>
+      <Grid item container>
+        <div style={{ height: '0.1vh', width: '100%' }}></div>
+      </Grid>
+    </Fragment>
   );
 }
 
