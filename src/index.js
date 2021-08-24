32a3e56b2bdf250c274464db63bbf058d069e349
Abdul Omar Faruq <18485958+mysticeti@users.noreply.github.com>
2021-08-24 01:57:58 +0100

    Add firebase auth (#45)
    Co-authored-by: Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { FirebaseAppProvider } from "reactfire";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCtaB2ksyFHzBuNkdzzqRkzBtL0nRHtcas",
+  authDomain: "react-sandbox-16efa.firebaseapp.com",
+  projectId: "react-sandbox-16efa",
+  storageBucket: "react-sandbox-16efa.appspot.com",
+  messagingSenderId: "360036954173",
+  appId: "1:360036954173:web:9f09006632fb1d9c657a54",
+  measurementId: "G-9BX5M169JT"
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FirebaseAppProvider firebaseConfig={firebaseConfig} suspense={true}>
+      <App />
+    </FirebaseAppProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
