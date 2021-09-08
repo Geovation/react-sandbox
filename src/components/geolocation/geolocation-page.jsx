a1154947f4b08e1388b11c4c6c67449d0890ffe6
Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>
2021-09-08 12:51:20 +0100

    Update geolocation page

--- src/components/geolocation/geolocation-page.jsx
+++ src/components/geolocation/geolocation-page.jsx
@@ -1,22 +1,23 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 function GeolocationPage() {
-  const [lat, setLat] = useState(null);
-  const [lng, setLng] = useState(null);
-  const [status, setStatus] = useState(null);
+  const [lat, setLat] = useState();
+  const [lng, setLng] = useState();
+  const [status, setStatus] = useState("No location found yet!");
 
   const getLocation = () => {
     if (!navigator.geolocation) {
-      setStatus("Geolocation is not supported by your browser");
+      setStatus("Geolocation is not supported by your browser.");
     } else {
       setStatus("Locating...");
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setStatus(null);
-          setLat(position.coords.latitude);
-          setLng(position.coords.longitude);
+          setStatus("");
+          setLat(position.coords.latitude.toFixed(7));
+          setLng(position.coords.longitude.toFixed(7));
         },
         () => {
           setStatus("Unable to retrieve your location.");
@@ -26,29 +27,34 @@ function GeolocationPage() {
   };
 
   return (
-    <Fragment>
-      <Grid
-        container
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        style={{ minHeight: "80vh" }}
-      >
-        <Grid item>
-          <Button variant="contained" color="primary" onClick={getLocation}>
-            Get Location
-          </Button>
-        </Grid>
-        <Grid item>
-          <h1>Coordinates</h1>
-        </Grid>
-        <Grid item>
-          <p>{status}</p>
-        </Grid>
-        <Grid item>{lat && <p>Latitude: {lat}</p>}</Grid>
-        <Grid item>{lng && <p>Longitude: {lng}</p>}</Grid>
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      style={{ minHeight: "80vh" }}
+      spacing={2}
+    >
+      <Grid item>
+        <Button variant="contained" color="primary" onClick={getLocation}>
+          Get Location
+        </Button>
       </Grid>
-    </Fragment>
+      <Grid item>
+        <Typography variant="h5">Coordinates</Typography>
+      </Grid>
+      {status && (
+        <Grid item>
+          <Typography>{status}</Typography>
+        </Grid>
+      )}
+      {lat && lng && (
+        <Grid item>
+          <Typography gutterBottom={true}>Latitude: {lat}</Typography>
+          <Typography>Longitude: {lng}</Typography>
+        </Grid>
+      )}
+    </Grid>
   );
 }
 
