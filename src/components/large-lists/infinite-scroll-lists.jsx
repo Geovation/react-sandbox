739975f4816303b3b696ea76a1b98b8c55ea8673
Joy Shan-Chun Kuo <shanchun.kuo@geovation.uk>
2021-09-14 15:14:38 +0100

    Update scroll height

--- src/components/large-lists/infinite-scroll-lists.jsx
+++ src/components/large-lists/infinite-scroll-lists.jsx
@@ -1,5 +1,5 @@
 import InfiniteScroll from "react-infinite-scroll-component";
-import React, { useState, Fragment } from "react";
+import React, { useState } from "react";
 import faker from "faker";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -35,36 +35,34 @@ function InfiniteScrollList() {
   };
 
   return (
-    <Fragment>
-      <div style={{ maxWidth: "85vw" }}>
-        <InfiniteScroll
-          dataLength={current.length}
-          next={getMoreData}
-          hasMore={hasMore}
-          height={700}
-          loader={<h4>Loading...</h4>}
-        >
-          {current &&
-            current.map((item, index) => (
-              <div key={index}>
-                <Box pt={3}>
-                  <Card>
-                    <div style={{ backgroundColor: "#CCFFCC" }}>
-                      <CardContent>
-                        <h3>{`${item.title}-${item.id}`}</h3>
-                        <p>{item.body}</p>
-                      </CardContent>
-                      <CardActions>
-                        <Button size="small">Learn More</Button>
-                      </CardActions>
-                    </div>
-                  </Card>
-                </Box>
-              </div>
-            ))}
-        </InfiniteScroll>
-      </div>
-    </Fragment>
+    <div style={{ maxWidth: "85vw" }}>
+      <InfiniteScroll
+        dataLength={current.length}
+        next={getMoreData}
+        hasMore={hasMore}
+        height={"80vh"}
+        loader={<h4>Loading...</h4>}
+      >
+        {current &&
+          current.map((item, index) => (
+            <div key={index}>
+              <Box pt={3}>
+                <Card>
+                  <div style={{ backgroundColor: "#CCFFCC" }}>
+                    <CardContent>
+                      <h3>{`${item.title}-${item.id}`}</h3>
+                      <p>{item.body}</p>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small">Learn More</Button>
+                    </CardActions>
+                  </div>
+                </Card>
+              </Box>
+            </div>
+          ))}
+      </InfiniteScroll>
+    </div>
   );
 }
 
