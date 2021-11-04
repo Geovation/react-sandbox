a2bdf01c45f378ed6ca5315d9fa24051fea8791e
Abdul Omar Faruq <abdulomarfaruq@gmail.com>
2021-11-04 09:54:18 +0000

    update large list

--- pages/large-lists.js
+++ pages/large-lists.js
@@ -1,3 +1,51 @@
+import React, { useState } from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import TabPanel from "../components/large-lists/tab-panel";
+import InfiniteScrollList from "../components/large-lists/infinite-scroll-lists";
+import ListsPagination from "../components/large-lists/lists-pagination";
+import Box from "@mui/material/Box";
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+}
+
 export default function LargeListsPage() {
-  return <div>Large List</div>;
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        bgcolor: "background.paper",
+        display: "flex",
+        height: "90vh",
+      }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Scrollable lists"
+        sx={{ borderRight: 1, borderColor: "divider" }}
+      >
+        <Tab label="Infinite scroll" {...a11yProps(0)} />
+        <Tab label="Pagination" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <InfiniteScrollList />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <ListsPagination />
+      </TabPanel>
+    </Box>
+  );
 }
